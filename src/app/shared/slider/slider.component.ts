8d48d5de3cd94f98b88f0fb40910d6e3a48fe47b
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-01-22 16:29:27 +0600

    add auto go to next slide && add auto return to first slide after take last one

--- src/app/shared/slider/slider.component.ts
+++ src/app/shared/slider/slider.component.ts
@@ -1,22 +1,35 @@
-import { Component } from '@angular/core';
-import { SLIDER_MOVE_NUMBER, slideGoType } from 'src/types';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subject, takeUntil, timer } from 'rxjs';
+import { SLIDER_MOVE_NUMBER, SLIDER_TIMER, slideGoType } from 'src/types';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.css']
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit, OnDestroy {
   public slides: string[] = [
     '../../../assets/img/slide-1.png',
     '../../../assets/img/slide-2.png',
     '../../../assets/img/slide-3.png',
     '../../../assets/img/slide-4.png',
   ]; // Array with images src. Now it's local, but we can get images form server
-  // TODO: сделай api Service и там имитируй получения от сервера, только из папки images и оставь коммент, что позже там можно добавить получение от сервера
 
   public types = slideGoType;
   public slidesPosition: number = 0;
+
+  private timer = timer(SLIDER_TIMER, SLIDER_TIMER); // 1st time after SLIDER_TIMER ms and then every SLIDER_TIMER ms
+  private destroy$ = new Subject<void>();
+
+  ngOnInit(): void {
+    // this.slides = this.apiService.getImages(); // TODO: сделай api Service и там имитируй получения от сервера
+    this.sliderGoByTimer();
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 
   public get arrowLeftDisabled(): boolean {
     return this.slidesPosition === 0;
@@ -35,5 +48,14 @@ export class SliderComponent {
         this.slidesPosition = this.slidesPosition + SLIDER_MOVE_NUMBER;
         break;
     }
+  }
+
+  private sliderGoByTimer(): void{
+    this.timer.pipe(takeUntil(this.destroy$)).subscribe(() => {
+      if (this.arrowRightDisabled) {
+        this.slidesPosition = 0;
+      }
+      this.go(slideGoType.next);
+    });
   }
 }
--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,4 @@ export enum slideGoType {
 }
 
 export const SLIDER_MOVE_NUMBER = 74; // number of rem to move for one slide
+export const SLIDER_TIMER = 5000; // time  to auto tern to next slide in ms
