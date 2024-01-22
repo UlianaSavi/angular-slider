fafd9ccc841e4b42bbdb678f8a6dd433fe67b09d
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-01-22 15:04:52 +0600

    add images for local slides && add styles for slider && add TODOS

--- src/app/shared/slider/slider.component.ts
+++ src/app/shared/slider/slider.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent {
-
+  public slides: string[] = [
+    '../../../assets/img/slide-1.png',
+    '../../../assets/img/slide-2.png',
+    '../../../assets/img/slide-3.png',
+    '../../../assets/img/slide-4.png',
+  ]; // Array with images src. Now it's local, but we can get images form server
+  // TODO: сделай api Service и там имитируй получения от сервера, только из папки images и оставь коммент, что позже там можно добавить получение от сервера
 }
